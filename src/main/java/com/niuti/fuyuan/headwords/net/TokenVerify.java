997d375e04d9c30d316686bbb09cc203f19da430
lifuyuan <lifuyuan33@gmail.com>
2015-08-11 20:46:19 +0800

    n

--- src/main/java/com/niuti/fuyuan/headwords/net/TokenVerify.java
+++ src/main/java/com/niuti/fuyuan/headwords/net/TokenVerify.java
@@ -3,6 +3,7 @@ package com.niuti.fuyuan.headwords.net;
 import android.text.TextUtils;
 
 import com.niuti.fuyuan.headwords.Config;
+import com.niuti.fuyuan.headwords.utils.Logger;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,6 +12,7 @@ import org.json.JSONObject;
  * Created by fuyuan on 2015/7/19.
  */
 public class TokenVerify {
+    protected String TAG;
     public TokenVerify(String token, final SuccessCallback successCallback, final FailCallback failCallback) {
         new NetConnection(Config.TOKEN_URL, HttpMethod.POST, new NetConnection.SuccessCallback() {
 
@@ -18,7 +20,9 @@ public class TokenVerify {
             public void onSuccess(String result) {
                 try {
                     JSONObject obj = new JSONObject(result);
-                    if (!TextUtils.isEmpty(obj.getString("result")) && obj.getString("result")=="true"){
+                    Logger.show(TAG, "TokenResultValue:" + obj.getString("result"));
+                    Logger.show(TAG, "TokenResult:" + obj.getString("result").equals("true"));
+                    if (!TextUtils.isEmpty(obj.getString("result")) && obj.getString("result").equals("true")){
                         if (successCallback!=null) {
                             successCallback.onSuccess(obj.getString("result"));
                         }
@@ -48,7 +52,7 @@ public class TokenVerify {
     }
 
     public static interface SuccessCallback{
-        void onSuccess(String code);
+        void onSuccess(String result);
     }
 
     public static interface FailCallback{
